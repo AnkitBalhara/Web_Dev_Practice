f885690356881a34ca78c62f83e0c269049e49c0
Ankit Balhara <ankitbalhara012@gmail.com>
2024-10-22 20:34:21 +0530

    Completed Location Tracker

--- Backend/LocationTracker/index.js
+++ Backend/LocationTracker/index.js
@@ -11,9 +11,16 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection",(socket)=>{
-  console.log("connected")
-})
+io.on("connection", (socket) => {
+  // console.log("connected");
+  socket.on("send-location", (data) => {
+    io.emit("recieve-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id);
+  });
+});
 
 app.get("/", (req, res) => {
   res.render("index");
--- Backend/LocationTracker/public/javascript/script.js
+++ Backend/LocationTracker/public/javascript/script.js
@@ -2,27 +2,45 @@ const socket = io();
 
 if (!navigator.geolocation) {
   alert("Sorry Your Device Doesn't support Location Setting..");
-} 
+}
 
-  navigator.geolocation.watchPosition(
-    (position) => {
-      // console.log(position)
-      const { latitude, longitude } = position.coords;
-      socket.emit("send-location", { latitude, longitude });
-    },
-    (error) => {
-      console.log("Error Occured in longitude aand latitude :- ", error);
-    },
-    {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
-    }
-  );
+navigator.geolocation.watchPosition(
+  (position) => {
+    // console.log(position)
+    const { latitude, longitude } = position.coords;
+    socket.emit("send-location", { latitude, longitude });
+  },
+  (error) => {
+    console.log("Error Occured in longitude aand latitude :- ", error);
+  },
+  {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  }
+);
 
+const map = L.map("map").setView([0, 0], 14);
 
-const map = L.map("map").setView([0,0],12)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Balhara Creation",
+}).addTo(map);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution:"Balhara Creation"
-}).addTo(map)
+const markers = {};
+
+socket.on("recieve-location", (data) => {
+  const { id, longitude, latitude } = data;
+  map.setView([latitude, longitude]);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
