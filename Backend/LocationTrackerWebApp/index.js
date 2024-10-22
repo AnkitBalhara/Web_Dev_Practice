827f9eca6860a30d9c7a95506513c546f486f451
Ankit Balhara <ankitbalhara012@gmail.com>
2024-10-22 22:52:14 +0530

    Update if user disconnected

--- Backend/LocationTrackerWebApp/index.js
+++ Backend/LocationTrackerWebApp/index.js
@@ -8,10 +8,13 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("connected");
+//   console.log("connected");
 
   socket.on("send-location", (data) => {
     io.emit("recieve-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnected", () => {
+    io.emit("user-disconnected", socket.id);
   });
 });
 
--- Backend/LocationTrackerWebApp/public/Javascript/script.js
+++ Backend/LocationTrackerWebApp/public/Javascript/script.js
@@ -22,29 +22,33 @@ navigator.geolocation.watchPosition(
 );
 
 // This below "L" comes from Leaflet script and setting Longitude and latitude for Earth center
-const map = L.map("map").setView([0,0],10)
+const map = L.map("map").setView([0, 0], 10);
 
 // Setting longitude and latitude for "India"
 // const map = L.map("map").setView([29, 76], 8);
 
 // This below line is to show map like colors in map(blue,green,...)
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution: "Balhara Creation"
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Balhara Creation",
 }).addTo(map);
 
 // Adding a marker object for our present/live location.
 const markers = {};
 
-socket.on("recieve-location",(data)=>{
-    const {id,latitude,longitude} = data;
-    map.setView([latitude,longitude],15)
+socket.on("recieve-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude], 15);
 
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
 
-    if(markers[id]){
-        markers[id].setLatLng([latitude,longitude])
-    }else{
-        markers[id]=L.marker([latitude,longitude]).addTo(map)
-    }
-})
-
-  
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
