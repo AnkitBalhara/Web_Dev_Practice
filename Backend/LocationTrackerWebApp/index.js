6ca93d42cdec1152fc50b58ec6b0eb84845bad6b
Ankit Balhara <ankitbalhara012@gmail.com>
2024-10-22 22:38:36 +0530

    Location Tracker Web App

---
+++ Backend/LocationTrackerWebApp/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("connected");
+
+  socket.on("send-location", (data) => {
+    io.emit("recieve-location", { id: socket.id, ...data });
+  });
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server Stared");
+});
---
+++ Backend/LocationTrackerWebApp/public/Javascript/script.js
@@ -0,0 +1,50 @@
+const socket = io();
+// console.log(socket)
+
+if (!navigator.geolocation) {
+  alert("Sorry Device Doesn,t support location functionality...");
+}
+
+navigator.geolocation.watchPosition(
+  (position) => {
+    const { latitude, longitude } = position.coords;
+    // This below line is to send the longitude and latitude to the frontend.
+    socket.emit("send-location", { latitude, longitude });
+  },
+  (error) => {
+    console.log("Error in longitude and latitude :-", error);
+  },
+  {
+    enableHighAccuracy: true,
+    maximumAge: 0,
+    timeout: 5000,
+  }
+);
+
+// This below "L" comes from Leaflet script and setting Longitude and latitude for Earth center
+const map = L.map("map").setView([0,0],10)
+
+// Setting longitude and latitude for "India"
+// const map = L.map("map").setView([29, 76], 8);
+
+// This below line is to show map like colors in map(blue,green,...)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution: "Balhara Creation"
+}).addTo(map);
+
+// Adding a marker object for our present/live location.
+const markers = {};
+
+socket.on("recieve-location",(data)=>{
+    const {id,latitude,longitude} = data;
+    map.setView([latitude,longitude],15)
+
+
+    if(markers[id]){
+        markers[id].setLatLng([latitude,longitude])
+    }else{
+        markers[id]=L.marker([latitude,longitude]).addTo(map)
+    }
+})
+
+  
