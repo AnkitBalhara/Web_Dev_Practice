4660f7d3b9b33a29c632f4baadef81a468b9ce8c
Ankit Balhara <ankitbalhara012@gmail.com>
2024-10-28 14:19:50 +0530

    Practice useState

--- React/HooksPractice/src/App.jsx
+++ React/HooksPractice/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+// const users = [
+//   { Name: "Ram", Age: 24 },
+//   { Name: "Lakshman", Age: 23 },
+//   { Name: "Hanuman", Age: 22 },
+//   { Name: "Bharat", Age: 21 },
+//   { Name: "Shatrugan", Age: 20 },
+// ];
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [users, setUsers] = useState([
+    { Name: "Ram", Age: 24 },
+    { Name: "Lakshman", Age: 23 },
+    { Name: "Hanuman", Age: 22 },
+    { Name: "Bharat", Age: 21 },
+    { Name: "Shatrugan", Age: 20 },
+  ]);
   return (
     <>
+      <h1>Jai Shree Ram</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {users.map((ele, index) => {
+          return (
+            <p key={index}>
+              {ele.Name} - {ele.Age} years old.
+            </p>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
