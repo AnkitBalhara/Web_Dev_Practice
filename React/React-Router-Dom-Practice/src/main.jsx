be77d0f550df47276959095cc698ea95ac469e56
Ankit Balhara <ankitbalhara012@gmail.com>
2024-11-02 16:04:17 +0530

    React  Router Practice

--- React/React-Router-Dom-Practice/src/main.jsx
+++ React/React-Router-Dom-Practice/src/main.jsx
@@ -2,31 +2,45 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
 import Root from "./Root.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path:"contact",
-        element:<Contact/>
-      }
-    ],
-  },
-]);
+// Method 1 to Create BrowseRouter...
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path:"contact",
+//         element:<Contact/>
+//       }
+//     ],
+//   },
+// ]);
+
+// Method 2 to create BrowseRouter...
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root/>}>
+      <Route path="" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
