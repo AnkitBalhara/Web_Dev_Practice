0f435c270eb66c2d4032e78b45b51654562712c0
Ankit Balhara <ankitbalhara012@gmail.com>
2024-11-04 21:41:56 +0530

    Simple Portfolio Web app

---
+++ React/PortFolio-Website/src/pages/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "../styles/Contact.css"
+
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <h2>Contact Me</h2>
+      <form>
+        <input type="text" placeholder="Your Name" required />
+        <input type="email" placeholder="Your Email" required />
+        <textarea placeholder="Your Message" required></textarea>
+        <button type="submit">Send Message</button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
