467ca2056d22bc65f310ab5da95766d1c2e58609
Ankit Balhara <ankitbalhara012@gmail.com>
2024-11-14 17:56:06 +0530

    Practice Socket.io

--- Backend/LocationTracker/index.js
+++ Backend/LocationTracker/index.js
@@ -1,29 +1,21 @@
 const express = require("express");
-const app = express();
 
+const app = express();
+const http = require("http");
 const path = require("path");
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname,"public")))
+app.set("view engine","ejs")
+
 
 const socketio = require("socket.io");
-const http = require("http");
-const server = http.createServer(app);
 const io = socketio(server);
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-
-io.on("connection", (socket) => {
-  // console.log("connected");
-  socket.on("send-location", (data) => {
-    io.emit("recieve-location", { id: socket.id, ...data });
-  });
-
-  socket.on("disconnect", () => {
-    io.emit("user-disconnected", socket.id);
-  });
-});
-
 app.get("/", (req, res) => {
-  res.render("index");
+  res.send("Jai Shree Ram");
 });
 
-server.listen(3000);
+server.listen("3000", () => {
+  console.log("Server Started");
+});
